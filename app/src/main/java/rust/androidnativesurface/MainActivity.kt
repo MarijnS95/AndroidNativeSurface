20c317d8d60e9a4fcc445b9cb62ef2fd7e3f63cc
Marijn Suijten <marijns95@gmail.com>
2022-05-30 23:34:16 +0200

    Use a SurfaceView for rendering

--- app/src/main/java/rust/androidnativesurface/MainActivity.kt
+++ app/src/main/java/rust/androidnativesurface/MainActivity.kt
@@ -2,10 +2,14 @@ package rust.androidnativesurface
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.SurfaceView
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val surfaceView: SurfaceView = findViewById(R.id.surface_view)
+        println("SurfaceView: ${surfaceView.holder.surface}")
     }
 }
