3179246ef971774e8576dd1ea33f417bc8e149c8
Marijn Suijten <marijns95@gmail.com>
2022-05-31 00:56:18 +0200

    Initial sample through glium

--- android_native_surface/src/lib.rs
+++ android_native_surface/src/lib.rs
@@ -1,3 +1,7 @@
+use glium::{
+    self, implement_vertex, index::PrimitiveType, program, uniform, Display, IndexBuffer, Surface,
+    VertexBuffer,
+};
 use jni::{
     objects::{JClass, JObject},
     JNIEnv,
@@ -13,14 +17,146 @@ fn render_to_native_window(window: NativeWindow) {
     let context = glutin::ContextBuilder::new()
         .build_windowed(&window)
         .unwrap();
+    let display = Display::from_gl_window(context).unwrap();
 
-    let context = unsafe { context.make_current() }.unwrap();
+    // building the vertex buffer, which contains all the vertices that we will draw
+    let vertex_buffer = {
+        #[derive(Copy, Clone)]
+        struct Vertex {
+            position: [f32; 2],
+            color: [f32; 3],
+        }
 
-    let gl = support::load(&context);
+        implement_vertex!(Vertex, position, color);
 
-    gl.draw_frame([1.0, 0.5, 0.7, 1.0]);
+        VertexBuffer::new(
+            &display,
+            &[
+                Vertex {
+                    position: [-0.5, -0.5],
+                    color: [0.0, 1.0, 0.0],
+                },
+                Vertex {
+                    position: [0.0, 0.5],
+                    color: [0.0, 0.0, 1.0],
+                },
+                Vertex {
+                    position: [0.5, -0.5],
+                    color: [1.0, 0.0, 0.0],
+                },
+            ],
+        )
+        .unwrap()
+    };
 
-    context.swap_buffers().unwrap();
+    // building the index buffer
+    let index_buffer =
+        IndexBuffer::new(&display, PrimitiveType::TrianglesList, &[0u16, 1, 2]).unwrap();
+
+    // compiling shaders and linking them together
+    let program = program!(&display,
+        140 => {
+            vertex: "
+                #version 140
+                uniform mat4 matrix;
+                in vec2 position;
+                in vec3 color;
+                out vec3 vColor;
+                void main() {
+                    gl_Position = vec4(position, 0.0, 1.0) * matrix;
+                    vColor = color;
+                }
+            ",
+
+            fragment: "
+                #version 140
+                in vec3 vColor;
+                out vec4 f_color;
+                void main() {
+                    f_color = vec4(vColor, 1.0);
+                }
+            "
+        },
+
+        110 => {
+            vertex: "
+                #version 110
+                uniform mat4 matrix;
+                attribute vec2 position;
+                attribute vec3 color;
+                varying vec3 vColor;
+                void main() {
+                    gl_Position = vec4(position, 0.0, 1.0) * matrix;
+                    vColor = color;
+                }
+            ",
+
+            fragment: "
+                #version 110
+                varying vec3 vColor;
+                void main() {
+                    gl_FragColor = vec4(vColor, 1.0);
+                }
+            ",
+        },
+
+        100 => {
+            vertex: "
+                #version 100
+                uniform lowp mat4 matrix;
+                attribute lowp vec2 position;
+                attribute lowp vec3 color;
+                varying lowp vec3 vColor;
+                void main() {
+                    gl_Position = vec4(position, 0.0, 1.0) * matrix;
+                    vColor = color;
+                }
+            ",
+
+            fragment: "
+                #version 100
+                varying lowp vec3 vColor;
+                void main() {
+                    gl_FragColor = vec4(vColor, 1.0);
+                }
+            ",
+        },
+    )
+    .unwrap();
+
+    // Here we draw the black background and triangle to the screen using the previously
+    // initialized resources.
+    //
+    // In this case we use a closure for simplicity, however keep in mind that most serious
+    // applications should probably use a function that takes the resources as an argument.
+    let draw = move || {
+        // building the uniforms
+        let uniforms = uniform! {
+            matrix: [
+                [1.0, 0.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0, 0.0],
+                [0.0, 0.0, 1.0, 0.0],
+                [0.0, 0.0, 0.0, 1.0f32]
+            ]
+        };
+
+        // drawing a frame
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 0.0, 0.0);
+        target
+            .draw(
+                &vertex_buffer,
+                &index_buffer,
+                &program,
+                &uniforms,
+                &Default::default(),
+            )
+            .unwrap();
+        target.finish().unwrap();
+    };
+
+    // Draw the triangle to the screen.
+    draw();
 }
 
 #[no_mangle]
