5d9b7e9747545de88ea23a0b2efdab6064c057f8
Marijn Suijten <marijns95@gmail.com>
2022-05-30 23:37:39 +0200

    Initial Surface integration with Rust project

--- app/src/main/java/rust/androidnativesurface/MainActivity.kt
+++ app/src/main/java/rust/androidnativesurface/MainActivity.kt
@@ -2,14 +2,40 @@ package rust.androidnativesurface
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.Surface
+import android.view.SurfaceHolder
 import android.view.SurfaceView
 
 class MainActivity : Activity() {
+    companion object {
+        init {
+            System.loadLibrary("android_native_surface")
+            init()
+        }
+
+        private external fun init()
+        external fun renderToSurface(surface: Surface)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val surfaceView: SurfaceView = findViewById(R.id.surface_view)
         println("SurfaceView: ${surfaceView.holder.surface}")
+        surfaceView.holder.addCallback(object : SurfaceHolder.Callback {
+            override fun surfaceCreated(holder: SurfaceHolder) {
+                println("SurfaceView created: ${holder.surface}")
+                renderToSurface(holder.surface)
+            }
+
+            override fun surfaceChanged(p0: SurfaceHolder, p1: Int, p2: Int, p3: Int) {
+//                    TODO("Not yet implemented")
+            }
+
+            override fun surfaceDestroyed(p0: SurfaceHolder) {
+//                    TODO("Not yet implemented")
+            }
+        })
     }
 }
